d62af591cbf517a9586b575ae85bf00cde93d2b2
theoxav <theoxxav@outlook.fr>
2025-01-27 18:16:13 +0100

    Add and update components: - Added News component with loader function in News.tsx - Added ErrorElement component for error handling - Updated main.tsx to include routing for News component - Updated customFetch utility for API requests

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,37 @@
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import HomeLayout from '@/pages/HomeLayout';
-import Apod from '@/pages/Apod';
-import Hubble from '@/pages/Hubble';
-import Landing from '@/pages/Landing';
-import News from '@/pages/News';
-import SpaceX from '@/pages/SpaceX';
-import Webb from '@/pages/Webb';
+import HomeLayout from "@/pages/HomeLayout";
+import Apod from "@/pages/Apod";
+import Hubble from "@/pages/Hubble";
+import Landing from "@/pages/Landing";
+import News, { newsPageLoader } from "@/pages/News";
+import SpaceX from "@/pages/SpaceX";
+import Webb from "@/pages/Webb";
 
-import './index.css';
+import "./index.css";
+import ErrorElement from "@/components/error/ErrorElement";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomeLayout />,
     children: [
       { index: true, element: <Landing /> },
-      { path: 'news', element: <News /> },
-      { path: 'webb', element: <Webb /> },
-      { path: 'spacex', element: <SpaceX /> },
-      { path: 'apod', element: <Apod /> },
-      { path: 'hubble', element: <Hubble /> },
+      {
+        path: "news",
+        element: <News />,
+        loader: newsPageLoader,
+        errorElement: <ErrorElement />,
+      },
+      { path: "webb", element: <Webb /> },
+      { path: "spacex", element: <SpaceX /> },
+      { path: "apod", element: <Apod /> },
+      { path: "hubble", element: <Hubble /> },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}></RouterProvider>
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router}></RouterProvider>,
 );
--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,4 +1,31 @@
+import { snapiCustomFetch } from "@/utils/customFetch";
+import { NewsResponse } from "@/utils/types";
+import { LoaderFunction, useLoaderData } from "react-router-dom";
+
+const newsParams = {
+  news_site_exclude: "SpacePolicyOnline.com",
+  limit: 20,
+  ordering: "-published_at",
+};
+
+export const newsPageLoader: LoaderFunction =
+  async (): Promise<NewsResponse | null> => {
+    try {
+      const formattedParams = { ...newsParams };
+      const response = await snapiCustomFetch.get<NewsResponse>("", {
+        params: formattedParams,
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
 const News = () => {
+  const { results } = useLoaderData() as NewsResponse;
+  console.log(results);
+
   return <div>News</div>;
 };
 
