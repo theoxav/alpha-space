9ab4b3860fcbe7a31d6dd28bdd0a43e1f30ddc0b
theoxav <theoxxav@outlook.fr>
2025-01-27 14:29:37 +0100

    Add the Header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,5 +1,29 @@
-const Header = () => {
-  return <header className="test">Header</header>;
+import { Link } from 'react-router-dom';
+import spaceImg from '@/assets/imgs/rocket-svgrepo-com.svg';
+import { ReactNode } from 'react';
+
+const Header = (): ReactNode => {
+  return (
+    <header className=" bg-black">
+      <div className="grid grid-cols-2 md:grid-cols-3 p-2 align-element">
+        <Link to="/" className="justify-self-start self-center">
+          <h1 className="mars-font text-lg pt-2 tracking-[0.8rem] text-white">
+            alphaSpace
+          </h1>
+        </Link>
+        <Link
+          to="/"
+          className="justify-self-end md:justify-self-center self-center"
+        >
+          <img
+            src={spaceImg}
+            alt="logo-top"
+            className="h-12 w-12 object-cover"
+          />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
