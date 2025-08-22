8e42e52af6357866a442a8a06ce785c0d029e34c
theoxav <theoxxav@outlook.fr>
2025-08-22 22:15:12 +0200

    feat(dropdown-menu): add Radix UI dropdown menu component and integrate into navbar

--- src/components/layouts/navbar/LinksDesktop.tsx
+++ src/components/layouts/navbar/LinksDesktop.tsx
@@ -3,18 +3,20 @@ import { NavLink } from "react-router-dom"
 
 const LinksDesktop = () => {
   return (
-    <div className="hidden lg:flex w-full flex gap-x-[5rem] justify-center items-center">
+    <div className="hidden lg:flex w-full flex gap-x-[5rem] justify-center items-center text-white">
       {links.map((link) => (
-        <NavLink 
-          key={link.label} 
-          to={link.ref} 
-          className={({isActive}) => `capitalize tracking-wide ${isActive ? 'underline text-xl' : ''}`}
+        <NavLink
+          key={link.label}
+          to={link.ref}
+          className={({ isActive }) =>
+            `capitalize tracking-wide ${isActive ? 'underline text-xl' : ''}`
+          }
         >
           {link.label}
         </NavLink>
       ))}
     </div>
-  )
+  );
 }
 
 export default LinksDesktop
--- src/components/layouts/navbar/LinksMobile.tsx
+++ src/components/layouts/navbar/LinksMobile.tsx
@@ -1,7 +1,40 @@
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+import links from '@/utils/links';
+import { AlignLeft } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
+
 const LinksMobile = () => {
   return (
-    <div className="">LinksMobile</div>
-  )
-}
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="block lg:hidden">
+        <Button variant="outline">
+          <AlignLeft />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="lg:hidden" align="start" sideOffset={10}>
+        {links.map((link) => (
+          <DropdownMenuItem key={link.label}>
+            <NavLink
+              to={link.ref}
+              className={({ isActive }) =>
+                `capitalize tracking-wide font-light ${
+                  isActive ? 'text-xl text-primary' : ''
+                }`
+              }
+            >
+              {link.label}
+            </NavLink>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
-export default LinksMobile
+export default LinksMobile;
