472396eec93ef95607c1418e229ce6d33e941da2
theoxav <theoxxav@outlook.fr>
2025-08-22 21:48:04 +0200

    feat(layout): enhance Header component with responsive design and logo

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,5 +1,29 @@
-const Header = () => {
-  return <div className="test">Header</div>;
+import { Link } from 'react-router-dom';
+import spaceImg from '../../assets/imgs/rocket.svg';
+import type { ReactNode } from 'react';
+
+const Header = (): ReactNode => {
+  return (
+    <header className="test bg-black">
+      <div className="align-element text-white grid grid-cols-2 md:grid-cols-3 p-2">
+        <Link to="/" className="justify-self-start self-center">
+          <h1 className="mars-font text-lg pt-2 tracking-[0.8rem]">
+            AlphaSpace
+          </h1>
+        </Link>
+        <Link
+          to="/"
+          className="justify-self-end md:justify-self-center self-center"
+        >
+          <img
+            src={spaceImg}
+            alt="AlphaSpace logo"
+            className="w-12 h-12 object-cover"
+          />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
